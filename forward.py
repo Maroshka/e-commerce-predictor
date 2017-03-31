30cee5f58cc749ae98c6815fce752b91ed2d8a18
Maroshka <maroshka202@gmail.com>
2017-04-01 00:13:07 +0300

    31-mar-2017

--- forward.py
+++ forward.py
@@ -1,19 +1,25 @@
 import numpy as np
 import pandas as pd
 
+def softmax(z, v):
+	expz = np.exp(z.dot(v))
+	h = expz / expz.sum(axis=1, keepdims=True)
+	return h
+	
+def sigmoid(a):
+	return 1 / (1 + np.exp(-a))
+
 def forward(x, w, v):
 	x2 = np.zeros((x.shape[0], x.shape[1]+1))
 	x2[:, 1:] = x
 	x2[:, 0] = 1
 
-	a = x2.dot(w)
-	z = 1 / (1 + np.exp(-a))
+	z = sigmoid(x2.dot(w)) 
 
 	z2 = np.zeros((z.shape[0], z.shape[1]+1))
 	z2[:, 1:] = z
 	z2[:, 0] = 1
 
-	expz = np.exp(z2)
-	h = expz / expz.sum(axis=1, keepdims=True)
+	h = softmax(z2, v)
 
 	return h, z
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 def cost(p, y):
 	c = -y*np.log(p)-(1-y)*np.log(1-p)
-	return c
+	return c.sum()
 
 def classification_rate(y, p):
 	return np.mean(y==p)
@@ -19,38 +19,56 @@ def derv_v(y, p, z):
 	return ret
 def derv_w(y, p, z, v, x):
 	x2 = np.zeros((x.shape[0], x.shape[1]+1))
-	x2[:, 1:] = z
+	x2[:, 1:] = x
 	x2[:, 0] = 1
 	dz = (p-y).dot(v.T)[:, 1:]*z*(1-z)
 	ret = x2.T.dot(dz)
 	return ret
+def oneHotEncoder(t, k):
+	T = np.zeros((t.shape[0], k))
+	for i in range(t.shape[0]):
+		T[i, int(t[i])] = 1
+	return T
 
 def main():
 	X, Y = get_data('ecom.csv')
 	K = int(Y.max())+1
+
+	Xtrain = X[:-100]
+	Ytrain = Y[:-100]
+	Ttrain = oneHotEncoder(Ytrain, K)
+
+	Xtest = X[-100:]
+	Ytest = Y[-100:]
+	Ttest = oneHotEncoder(Ytest, K)
+
 	N, D = X.shape
 	M = 3
 	w = np.random.rand(D+1, M)
 	v = np.random.rand(M+1, K)
 
-	T = np.zeros((N, K))
-	for i in range(N):
-		T[i, int(Y[i])] = 1
 	cs = []
 	rs = []
-	alpha = 0.00005
-	for i in range(1000):
-		p, z = forward(X, w, v)
-		c = cost(p, T)
+	alpha = 0.000005
+	for i in range(100000):
+		p, z = forward(Xtrain, w, v)
+		c = cost(p, Ttrain)
 		cs.append(c)
-		r = classification_rate(Y, p.argmax(axis=1))
+		r = classification_rate(Ytrain, p.argmax(axis=1))
 		rs.append(r)
-		v -= alpha*derv_v(T, p, z)
-		w -= alpha*derv_w(T, p, z, v, xs)
+		v -= alpha*derv_v(Ttrain, p, z)
+		w -= alpha*derv_w(Ttrain, p, z, v, Xtrain)
+		print "cost: ",c,", classification rate: ",r
 	plt.plot(cs)
 	plt.show()
 	plt.plot(rs)
 	plt.show()
+
+	p, z = forward(Xtest, w, v)
+	c = cost(p, Ttest)
+	r = classification_rate(Ytest, p.argmax(axis=1))
+
+	print "Model's accuracy: ",r*100,"%, with cost of: ", c
 
 if __name__ == '__main__':
 	main()
